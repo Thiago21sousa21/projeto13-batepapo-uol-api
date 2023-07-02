d0a59314779fae82856321d20df1638da1145d2b
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-07-02 17:23:26 -0300

    lista as mensagens enviadas

--- src/app.js
+++ src/app.js
@@ -72,35 +72,54 @@ app.post('/participants', async(req, res)=>{
 //lista de participantes
 app.get('/participants', (req, res)=>{
     db.collection('participants').find().toArray()
-        .then((participants) => res.send(participants));
+        .then((participants) => res.send(participants))
+        .catch((err)=> res.status(500).send(err.message))
 });
 
 
 // envia uma mensagem
-app.post('/messages', (req, res)=>{
-
+app.post('/messages', async(req, res)=>{
     const messageSchema = joi.object({
-        from: joi.string().min(1).required(),
-        to: joi.string().min(1).allow('Todos').required(),
+        to: joi.string().min(1).required(),
         text: joi.string().min(1).required(),
-        type: joi.string().min(1).allow('message', 'private_message').required(),
+        type: joi.string().min(1).valid('message', 'private_message').required(),
     }); 
     const {to, text, type} = req.body;
+    const {user} = req.headers;
     const validation = messageSchema.validate(req.body);
+    const userValidation =  await db.collection('participants').findOne({name: user});
     if(validation.error){
         const errors = validation.error.details.map((detail) => detail.message);
         return res.status(422).send(errors);
     }
-    const {from} = req.header;
-    db.collection('messages').insertOne({to, text, type, time: dayjs().format('hh:mm:ss') });
-    res.send('ok');
+    if(!userValidation)return res.sendStatus(422);
+    try{
+        await db.collection('messages').insertOne({user, to, text, type, time: dayjs().format('hh:mm:ss') });
+        res.send('ok');    
+    }catch(error){
+        res.status(500).send(error.message);
+    }
 });
 
 
 //lista as mensagens enviadas
-app.get('/messages', (req, res)=>{
-    db.collection( 'messages').find().toArray()
-        .then((messages)=>res.send(messages));
+app.get('/messages', async(req, res)=>{
+    const {user} = req.headers;
+    const {limit} = req.query;
+
+    const validLimit = !limit || limit && limit > 0;
+    const validUser = await db.collection('participants').findOne({name: user});
+    console.log( validLimit, validUser);
+    if(!validLimit || !validUser )return res.sendStatus(422);
+
+    db.collection( 'messages').find({ $or: [{ from: user }, { to: user }, { to: "Todos" }] })
+    .toArray()
+        .then((messages)=>{
+            console.log(messages);
+            if(limit) return res.send(messages.slice(-limit));
+            res.send(messages)
+        })
+        .catch(error => res.status(500).send(error));
 })
 
 app.listen( PORT ,()=>{ console.log(`RUNING PORT ${PORT}`)});
